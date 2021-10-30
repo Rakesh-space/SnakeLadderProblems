044006b10beaf360e6293791346273bc01d373f9
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 13:20:23 +0530

    Adding UC3 only check ladder

--- SnakeLadderProblems/Program.cs
+++ SnakeLadderProblems/Program.cs
@@ -6,8 +6,8 @@ namespace SnakeLadderProblems
     {
         static void Main(string[] args)
         {
-            UC2GetNo1to6 singlePlayer=new UC2GetNo1to6();
-            singlePlayer.snakeLadderUC2();
+            UC3CheckLadder singlePlayer=new UC3CheckLadder();
+            singlePlayer.snakeLadderUC3();
         }
     }
 }
